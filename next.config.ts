acfec23fed50ac9bb66d42dd66dcbb6065dc6ecb
NicT5 <nicctang@gmail.com>
2026-02-01 23:36:10 +0800

    Changes to support github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "export",
+    basePath: "/nicewebsite",
 };
 
 export default nextConfig;
