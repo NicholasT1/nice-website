ca956d2fee65dc369ad643320c0627a7bd6349b7
NicT5 <nicctang@gmail.com>
2026-02-01 23:55:06 +0800

    Set base path for next

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
-    basePath: "/nicewebsite",
+    basePath: "/nice-website",
 };
 
 export default nextConfig;
